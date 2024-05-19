1bc96c146cc17d7023dd007402c1b336a3f48853
ek5em <pro-100-dim@mail.ru>
2024-05-19 12:33:43 +0400

    [ADD] Activity page

--- js/pages/renderError.js
+++ js/pages/renderError.js
@@ -2,7 +2,7 @@ const renderError = () => {
     const main = document.getElementById("main-content");
     main.innerHTML = `
         <div class="container">
-            "<h2>Page not found</h2>"
+            <h2>Page not found</h2>
         </div>
     `;
 };
